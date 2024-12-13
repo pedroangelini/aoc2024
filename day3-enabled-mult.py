6cfc64b48a3347af4e6729082bcd06909f5a71c6
pedroangelini <72536471+pedroangelini@users.noreply.github.com>
2024-12-13 21:25:36 +0100

    day3 yay!

--- day3-enabled-mult.py
+++ day3-enabled-mult.py
@@ -1,21 +1,30 @@
 import re
 from utils.input_utils import get_input_from_args, str_seq_to_int_tup
 
-input=get_input_from_args()
-mult_matcher=re.compile(r"mul\((\d+),(\d+)\)")
+# import rich
 
-disabled_pat=re.compile(r"don\'t\(\).*do\(\)")
-clean_input = disabled_pat.sub('',input)
 
-#print(disabled_pat.findall(input))
-#print(input)
-#print(clean_input)
+def main() -> int:
+    input = get_input_from_args()
+    mult_matcher = re.compile(r"mul\((\d+),(\d+)\)")
 
-acum =0
+    disabled_pat = re.compile(r"don\'t\(\)(?:.|\n)*?(?:do\(\)|$)")
+    clean_input = disabled_pat.sub("", input)
 
-for match in mult_matcher.findall(clean_input):
-  a,b=str_seq_to_int_tup(match)
-  print(f"{a=}, {b=}, {(a*b)=}")
-  acum += a*b
+    # rich.print(disabled_pat.findall(input))
+    # print(input)
+    # print(clean_input)
 
-print(acum)
+    acum = 0
+
+    for match in mult_matcher.findall(clean_input):
+        a, b = str_seq_to_int_tup(match)
+        print(f"{a=}, {b=}, {(a*b)=}")
+        acum += a * b
+
+    print(acum)
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
